2f4956764395f4c67370048c18cccbf363bb5af9
Francesco Pastore <116507677+francescopasta@users.noreply.github.com>
2025-01-15 10:46:06 +0100

    FristPerson_Controller_Francesco

---
+++ PROJECT/Hell_sClub/Assets/PersonalFolders/FranFolder/FranScript/FirstPersonPlayerCamera.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonPlayerCamera : MonoBehaviour
+{
+    public Transform playerBody;
+    public float mouseSensitivity = 100f;
+
+    private float verticalRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; 
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        verticalRotation -= mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+
+    }
+}
+
