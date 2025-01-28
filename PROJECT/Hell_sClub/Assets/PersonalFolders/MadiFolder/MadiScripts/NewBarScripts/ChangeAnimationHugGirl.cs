9f97a316887744550cc7334cbb4217e9fdb2443a
Magdalena Nikolova <maddynikolova@gmail.com>
2025-01-28 01:19:36 +0100

    AnimInProgress

--- PROJECT/Hell_sClub/Assets/PersonalFolders/MadiFolder/MadiScripts/NewBarScripts/ChangeAnimationHugGirl.cs
+++ PROJECT/Hell_sClub/Assets/PersonalFolders/MadiFolder/MadiScripts/NewBarScripts/ChangeAnimationHugGirl.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class ChangeAnimationHugGirl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject targetObject; // Reference to the target object with the Animator
+    private Animator targetAnimator;
+
     void Start()
     {
-        
+        // Find the Animator on the target object
+        if (targetObject != null)
+        {
+            targetAnimator = targetObject.GetComponent<Animator>();
+            if (targetAnimator == null)
+            {
+                Debug.LogError("Animator not found on the target object!");
+            }
+        }
+        else
+        {
+            Debug.LogError("Target object not assigned!");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Function triggered by the animation event
+    public void ChangeTargetAnimationState(string newState)
     {
-        
+        if (targetAnimator != null)
+        {
+            targetAnimator.Play(newState); // Play the specified animation
+        }
+        else
+        {
+            Debug.LogWarning("Animator on the target object is not assigned or found!");
+        }
     }
 }
