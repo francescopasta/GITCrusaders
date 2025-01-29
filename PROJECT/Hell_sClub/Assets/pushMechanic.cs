a8ce87bc9e58a98798ee1fd6274edfd5bbad3c60
Dheerrrai <dheerrrai@gmail.com>
2025-01-29 01:08:04 +0100

    loreshit

--- PROJECT/Hell_sClub/Assets/pushMechanic.cs
+++ PROJECT/Hell_sClub/Assets/pushMechanic.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class pushMechanic : MonoBehaviour
 {
@@ -16,6 +17,18 @@ public class pushMechanic : MonoBehaviour
     public bool audioPlaying;
     public AudioSource huggersPushedOffAudio;
     // Update is called once per frame
+    public GameObject EKeyPress;
+    public GameObject EKeyOpen;
+    public GameObject Bar1;
+    public GameObject Bar2;
+    public Image Bar1Fill;
+    public Image Bar2Fill;
+    // Update is called once per frame
+    private void Start()
+    {
+        SetUI(false);
+        EKeyOpen.SetActive(false);
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) && huggerScript.attachedHuggers > 0)
@@ -27,9 +40,33 @@ public class pushMechanic : MonoBehaviour
             buttonMashRequirement++;
             pushing = true;
             playerAnimator.SetTrigger("isUsingPush");
+            EKeyOpen.SetActive(false);
+        }
+        if (Input.GetKeyUp(KeyCode.E) && pushing)
+        {
+            EKeyOpen.SetActive(true);
+        }
+        if (huggerScript.attachedHuggers > 0)
+        {
+
+            SetUI(true);
         }
 
     }
+    private void LateUpdate()
+    {
+        if (pushing)
+        {
+            Bar1Fill.fillAmount = (float)buttonMashRequirement / totalMashRequirement;
+            Bar2Fill.fillAmount = (float)buttonMashRequirement / totalMashRequirement;
+        }
+        else
+        {
+            Bar1Fill.fillAmount = 0;
+            Bar2Fill.fillAmount = 0;
+        }
+    }
+
     private void FixedUpdate()
     {
         if (pushing)
@@ -46,6 +83,8 @@ public class pushMechanic : MonoBehaviour
                     currentMashTimer = 0;
                     pushing = false;
                     huggersPushedOffAudio.Play();
+                    SetUI(false);
+                    EKeyOpen.SetActive(false);
                 }
             }
             if (currentMashTimer >= totalMashTimer && buttonMashRequirement < totalMashRequirement)
@@ -53,9 +92,26 @@ public class pushMechanic : MonoBehaviour
                 pushing = false;
                 buttonMashRequirement = 0;
                 currentMashTimer = 0;
+                Bar1.SetActive(false);
+                Bar2.SetActive(false);
+                Bar1Fill.enabled = false;
+                Bar2Fill.enabled = false;
+                //DeactivateBar
+
             }
         }
+
     }
-    
+    public void SetUI(bool Value)
+    {
+        EKeyPress.SetActive(Value);
+
+        Bar1.SetActive(Value);
+        Bar2.SetActive(Value);
+        Bar1Fill.enabled = Value;
+        Bar2Fill.enabled = Value;
+
+    }
+
 
 }
