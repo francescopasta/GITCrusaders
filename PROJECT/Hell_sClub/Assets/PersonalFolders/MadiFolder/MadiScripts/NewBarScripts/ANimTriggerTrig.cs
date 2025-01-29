8da777d9302ae8bd6aea9a99a2ba48f123b37c2c
Magdalena Nikolova <maddynikolova@gmail.com>
2025-01-29 19:48:08 +0100

    AnimFInish

--- PROJECT/Hell_sClub/Assets/PersonalFolders/MadiFolder/MadiScripts/NewBarScripts/ANimTriggerTrig.cs
+++ PROJECT/Hell_sClub/Assets/PersonalFolders/MadiFolder/MadiScripts/NewBarScripts/ANimTriggerTrig.cs
@@ -6,6 +6,7 @@ public class ANimTriggerTrig : MonoBehaviour
 {
     public Animator targetAnimator; // Reference to the target Animator
     public string triggerParameterName; // Name of the trigger parameter to trigger
+    public Collider targetCollider; // Reference to the collider to disable
 
     // This function will be called as an animation event
     public void TriggerAnimation()
@@ -17,6 +18,15 @@ public class ANimTriggerTrig : MonoBehaviour
         else
         {
             Debug.LogWarning("Target Animator or Parameter Name not set on " + gameObject.name);
+        }
+
+        if (targetCollider != null)
+        {
+            targetCollider.enabled = false;
+        }
+        else
+        {
+            Debug.LogWarning("Target Collider not set on " + gameObject.name);
         }
     }
 }
