d7d7bea5de3212a9a9e0831f687832722212d975
Lyubomir Kolev <lubokolev14@gmail.com>
2025-01-29 16:16:23 +0100

    GiantSFXScript

---
+++ PROJECT/Hell_sClub/Assets/GiantSFX.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiantSFX : MonoBehaviour
+{
+    public List<AudioSource> grunts;
+    private float timeBetweenGrunts;
+    public float minTime;
+    public float maxTime;
+    private int randomGrunt;
+    private bool canGrunt;
+    private void Update()
+    {
+        if (canGrunt) 
+        {
+            StartCoroutine(Grunt());
+        }
+    }
+    public IEnumerator Grunt() 
+    {
+        canGrunt = false;
+        timeBetweenGrunts = Random.Range(minTime, maxTime);
+        randomGrunt = Random.Range(0, grunts.Count - 1);
+        grunts[randomGrunt].Play();
+        yield return new WaitForSeconds(timeBetweenGrunts);
+        canGrunt = true;
+    }
+}
